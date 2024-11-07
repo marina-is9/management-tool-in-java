57e27721d63c333a6ef79b9030b9d40046844096
Marina Martirosyan <122169329+marina-is9@users.noreply.github.com>
2024-11-07 16:08:39 +0900

    Create Decription.java

---
+++ App/valueobjects/Decription.java
@@ -0,0 +1,14 @@
+package com.example.project.valueobjects;
+
+public class Description {
+
+    private final String description;
+
+    public Description(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
