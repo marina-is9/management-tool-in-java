333bbb0541ee75d8e641e547badc13dd8e6e18ad
Marina Martirosyan <122169329+marina-is9@users.noreply.github.com>
2024-11-07 16:08:21 +0900

    Create Name.java

---
+++ App/valueobjects/Name.java
@@ -0,0 +1,14 @@
+package com.example.project.valueobjects;
+
+public class Name {
+
+    private final String name;
+
+    public Name(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
