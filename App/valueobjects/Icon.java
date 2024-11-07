80606bb2518106a58f7afa8537d4642aff279ca5
Marina Martirosyan <122169329+marina-is9@users.noreply.github.com>
2024-11-07 16:08:55 +0900

    Create Icon.java

---
+++ App/valueobjects/Icon.java
@@ -0,0 +1,14 @@
+package com.example.project.valueobjects;
+
+public class Icon {
+
+    private final String icon;
+
+    public Icon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+}
