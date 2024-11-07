479ac956df7abb6495b083e2a4ee6f511f0bbb44
Marina Martirosyan <122169329+marina-is9@users.noreply.github.com>
2024-11-07 16:08:04 +0900

    Create ProjectId.java

---
+++ App/valueobjects/ProjectId.java
@@ -0,0 +1,14 @@
+package com.example.project.valueobjects;
+
+public class ProjectId {
+
+    private final Long id;
+
+    public ProjectId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
