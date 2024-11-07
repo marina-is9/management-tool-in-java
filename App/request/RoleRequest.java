0272a75c021558f5bcd2ea2fc5c189a67bf2b3b1
Marina Martirosyan <122169329+marina-is9@users.noreply.github.com>
2024-11-07 16:04:05 +0900

    Create RoleRequest.java

---
+++ App/request/RoleRequest.java
@@ -0,0 +1,25 @@
+package com.example.project.request;
+
+public class RoleRequest {
+
+    private Long id;
+    private String name;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
